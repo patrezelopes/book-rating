fd301d998fc2937299a1821ac82f13e9c45d3501
Patreze Lopes <patreze.lopes@faci.ly>
2022-08-06 21:31:02 -0300

    including alembic files

---
+++ alembic/versions/183613ee2387_first.py
@@ -0,0 +1,24 @@
+"""first
+
+Revision ID: 183613ee2387
+Revises: 
+Create Date: 2022-08-06 12:25:01.345692
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '183613ee2387'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
